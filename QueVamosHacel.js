b3d3f1c9204dfb3712f97b2763524cc44b435516
Omig12 <israelodilan@gmail.com>
2016-04-09 15:28:47 -0400

    first works on js

--- QueVamosHacel.js
+++ QueVamosHacel.js
@@ -2,4 +2,24 @@
 
 document.getElementById("demo").innerHTML = "Hello World";
 
-function wight()
+function htmlEdit() {
+	document.getElementById("demo").innerHTML = "Hello World";
+	
+	// Equals an ordered list of ints (1 to k) were k is the number of users. 
+	var id;
+	//
+	var list;
+	//  One Choice per User
+	var choice [];
+	var user (Id, );	
+	var ranking 
+	
+	// Needs to take a list of ranked elements
+	function getList() {
+	
+		return lists;
+	}
+	function weight(lists, )  {
+		
+	}	
+}
