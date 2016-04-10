702e49a5efa18d0065967d63972daced060f01d7
Omig12 <israelodilan@gmail.com>
2016-04-09 20:30:41 -0400

    Commented most stuff

--- QueVamosHacel.js
+++ QueVamosHacel.js
@@ -1,24 +1,34 @@
+/* Javascript back-end for Quevamosahacer? */
+
+//  Stores amount of friends
 var numUser = 0;
+// Stores posible choices list
 var options = new Array;
 
+// General input helper
 function get_input(item) {
 	var item = readline();
 	return item;	
 }
 
+// Get number of users
 print("Enter amount of users: ");	
 numUser = get_input();
+
 print("Enter your option: ");
 for (var i = 0; i < numUser; i++) {
+	console.log(i + ") ");	
 	options[i] = get_input();
 }
 
+/*borrar*/
 function msg(){
-
 
 	print("Filling you with malanga!");}
 msg();
+/*borrar*/
 
+// fill array of each users preference
 var userArray = new Array(numUser);
 for (var i = 0; i < numUser; i++) {
 	userArray[i] = new Array(numUser);
@@ -27,12 +37,9 @@ for (var i = 0; i < numUser; i++) {
 	}
 }
 
-function stubs() {
-	print("Enter array index: ");
-	print(id[get_input()]);
-	print( "Total num users: " + numUser);	}
-
+/* stubs */
 print("\nuser preference: ");
 for (var i = 0; i < numUser; i++) {
 	print(userArray[i]);
 }
+
