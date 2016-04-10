185718116ad8d982449f8c515bb372ca250a5dae
jreyes <josereyes5396@outlook.com>
2016-04-09 23:24:38 -0400

    Add unfinished voting tally

--- QueVamosHacel.js
+++ QueVamosHacel.js
@@ -8,6 +8,8 @@ var options = new Array;
 var budget = undefined;
 // Stores max distance
 var distance = undefined;
+// Stores winner for IRV do-while
+var winner = undefined;
 
 // General input helper
 function get_input() {
@@ -22,14 +24,17 @@ numUser = get_input();
 // Gets the options for user
 print("Enter your option: ");
 for (var i = 0; i < numUser; i++) {
-	print(i + ") ");
-	options[i] = get_input();
+    print(i + ") ");
+    options[i] = new Array(2);
+	options[i][0] = get_input();
+    options[i][1] = 0;
 }
 
 /*borrar*/
 function msg(){
 
-	print("Filling you with malanga!");}
+	print("Filling you with malanga!");
+}
 msg();
 /*borrar*/
 
@@ -41,6 +46,21 @@ for (var i = 0; i < numUser; i++) {
 		userArray[i][k] = "Malanga";
 	}
 }
+
+do {
+    for (var i in userArray) {
+        for (var j in userArray[i]) {
+            if (userArray[i][0] == options[j][0]) { // go over options and increment per first choice
+                 options[j][1] += 1; // where incrementation is actually done
+                 if (options[j][1] >= numUser / 2) { // check whether there is a choice with majority of votes
+                     winner = options[j][0]; // store winner
+                 }
+            }
+        }
+    }
+} while (winner == undefined)
+
+print(winner)
 
 /* stubs */
 print("\nuser preference: ");
